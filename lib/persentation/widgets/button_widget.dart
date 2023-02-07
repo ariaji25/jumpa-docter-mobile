664f4a335e60bf3f91d05271c4aaa88dde1cf857
Muhammad Ziad Alfian <muhammadziadalfian07@gmail.com>
2023-02-07 12:51:35 +0800

    kanban-board#181 Search Dokter Page

--- lib/persentation/widgets/button_widget.dart
+++ lib/persentation/widgets/button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jd_mobile/common/extensions/context_ext.dart';
 import 'package:jd_mobile/common/resources/colors.dart';
 
 class ButtonWidget extends StatelessWidget {
@@ -58,6 +59,54 @@ class OutlineedButtonWidget extends StatelessWidget {
         },
         child: Text(title),
       ),
+    );
+  }
+}
+
+class SearchButtonWidget extends StatelessWidget {
+  const SearchButtonWidget({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+  });
+
+  final String title;
+  final String subtitle;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title),
+        context.sbHeight(size: 9),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: AppColors.primaryColor,
+              side: const BorderSide(
+                color: AppColors.primaryColor,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            onPressed: () {
+              onTap();
+            },
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                subtitle,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
