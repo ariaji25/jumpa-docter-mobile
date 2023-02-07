ac353d56bd5e224d2d67b9cb6c2bec999efcf5af
muhammad1510520253 <tkjmuhammad@gmail.com>
2023-02-07 15:07:37 +0800

    kanban-board#170 Add Bottom navigation bar

---
+++ lib/persentation/pages/base/base_page.dart
@@ -0,0 +1,341 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:jd_mobile/common/theme/theme.dart';
+import 'package:jd_mobile/persentation/pages/home/home_page.dart';
+import 'package:jd_mobile/persentation/widgets/logo_widget.dart';
+
+import '../../../common/extensions/context_ext.dart';
+import '../../../common/extensions/padding_ext.dart';
+import '../../../common/resources/assets.dart';
+import '../../../common/resources/colors.dart';
+import '../../../common/resources/size.dart';
+import '../../widgets/app_bars.dart';
+import '../auth/login_page.dart';
+
+class BasePage extends StatefulWidget {
+  static const routeName = "/BasePage";
+
+  const BasePage({Key? key}) : super(key: key);
+
+  @override
+  BasePageState createState() => BasePageState();
+}
+
+class BasePageState extends State<BasePage> {
+  int _currentIndex = 0;
+  final _screens = [
+    const HomePage(),
+    const HomePage(),
+    const HomePage(),
+    const HomePage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+/*    Future.delayed(const Duration(milliseconds: 300), () async {
+      await _patient.detailPatientByNRM();
+      _isInvalidToken();
+      _isNewPatient(context);
+    });*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(_currentIndex != 0 ? 90 : 150),
+        child: AppsBar(
+          rectangleBorder: const RoundedRectangleBorder(),
+          elevation: 0,
+          flexibleSpaceBar: FlexibleSpaceBar(
+            centerTitle: true,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: paddingOnly(top: 40.0),
+                        child: LogoWidget.logoWhite,
+                      ),
+
+                      /// APPBAR ITEMS
+                      if (_currentIndex != 3)
+                        Padding(
+                          padding: paddingOnly(top: 40.0, right: 16.0),
+                          child: Row(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  /*Obx(() {
+                                    return */
+                                  Text(
+                                    "👋Hi, Muh",
+                                    style: AppTheme.subtitle.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  // }),
+                                  Flexible(
+                                    child: Text(
+                                      "Bagaimana kabarmu hari ini?",
+                                      style: AppTheme.subtitle
+                                          .copyWith(fontSize: 14),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 5),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    LoginPage.routeName,
+                                    (route) => false,
+                                  );
+                                },
+                                onDoubleTap: () {
+                                  // if (F.appFlavor == Flavor.DEV) {
+                                  //   alice.showInspector();
+                                  // }
+                                },
+                                child: const Icon(
+                                  Icons.account_circle,
+                                  size: 34,
+                                  color: AppColors.whiteColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: _currentIndex == 0,
+                  child: context.sbHeight(size: 23),
+                ),
+                Visibility(
+                  visible: _currentIndex == 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: SizeConstants.margin),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamedAndRemoveUntil(
+                                context,
+                                LoginPage.routeName,
+                                (route) => false,
+                              );
+                            },
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: AppColors.primaryColorDarkColor
+                                    .withOpacity(0.4),
+                              ),
+                              child: Row(
+                                children: [
+                                  context.sbWidth(size: 5),
+                                  const Icon(
+                                    FeatherIcons.search,
+                                    color: Color(0XFF718096),
+                                    size: 20,
+                                  ),
+                                  context.sbWidth(size: 10),
+                                  Text(
+                                    "Cari dokter",
+                                    style: AppTheme.bodyText.copyWith(
+                                        color: const Color(0XFF718096)),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        // context.sbWidth(size: 11),
+                        // Container(
+                        //   width: 50,
+                        //   height: 47,
+                        //   decoration: BoxDecoration(
+                        //     color:
+                        //         AppColors.primaryColorDarkColor.withOpacity(0.4),
+                        //     borderRadius: BorderRadius.circular(6),
+                        //   ),
+                        //   child: const Icon(
+                        //     FeatherIcons.settings,
+                        //     color: AppColors.whiteColor,
+                        //   ),
+                        // ),
+                        context.sbWidth(size: 11),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushNamedAndRemoveUntil(
+                              context,
+                              LoginPage.routeName,
+                              (route) => false,
+                            );
+                          },
+                          child: Container(
+                            width: 50,
+                            height: 47,
+                            decoration: BoxDecoration(
+                              color: AppColors.primaryColorDarkColor
+                                  .withOpacity(0.4),
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: const Icon(
+                              FeatherIcons.bell,
+                              color: AppColors.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: _screens[_currentIndex],
+      ),
+      bottomNavigationBar: Container(
+        height: 75,
+        decoration: const BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+              spreadRadius: 0,
+              blurRadius: 1,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: _updateIndex,
+          selectedItemColor: AppColors.primaryColor,
+          unselectedItemColor: const Color(0XFFA0AEC0),
+          selectedFontSize: 13,
+          unselectedFontSize: 13,
+          iconSize: 30,
+          backgroundColor: AppColors.whiteColor,
+          items: [
+            BottomNavigationBarItem(
+              label: "Beranda",
+              activeIcon: SvgPicture.asset(
+                "${Assets.iconsPath}/house.svg",
+                width: 20,
+                height: 20,
+                color: AppColors.primaryColor,
+              ),
+              icon: SvgPicture.asset(
+                "${Assets.iconsPath}/house.svg",
+                width: 20,
+                height: 20,
+                color: const Color(0XFFA0AEC0),
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: "Pesan",
+              activeIcon: SvgPicture.asset("${Assets.iconsPath}/chat.svg",
+                  width: 20, height: 20, color: AppColors.primaryColor),
+              icon: SvgPicture.asset("${Assets.iconsPath}/chat.svg",
+                  width: 20, height: 20, color: const Color(0XFFA0AEC0)),
+            ),
+            BottomNavigationBarItem(
+              label: "Janji Temu",
+              activeIcon: SvgPicture.asset(
+                "${Assets.iconsPath}/cart.svg",
+                width: 20,
+                height: 20,
+                color: AppColors.primaryColor,
+              ),
+              icon: SvgPicture.asset("${Assets.iconsPath}/cart.svg",
+                  width: 20, height: 20, color: const Color(0XFFA0AEC0)),
+            ),
+            const BottomNavigationBarItem(
+              label: "Profil",
+              activeIcon: Icon(
+                FeatherIcons.user,
+                size: 20,
+              ),
+              icon: Icon(
+                FeatherIcons.user,
+                size: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _updateIndex(int value) {
+    setState(() {
+      _currentIndex = value;
+    });
+  }
+
+  void _isNewPatient(BuildContext context) {
+    /*if (_patient.isNewPatient.isTrue && _patient.isInvalidToken.isFalse) {
+      confirmModal(
+        context,
+        "Pasien Baru",
+        "Selamat datang di JumpaDokter. \nKami tidak menemukan 'Rekam Medis' anda, lengkapi sekarang?",
+        onOk: () {
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            LoginPage.routeName,
+                (route) => false,
+          );
+        },
+        okText: "Sekarang",
+        cancelText: "Nanti",
+      );
+    }*/
+  }
+
+  void _isInvalidToken() async {
+    /* printDebug("CEKING _isInvalidToken ${_patient.isInvalidToken.value}");
+    if (_patient.isInvalidToken.value == true) {
+      await _otp.logout().then((value) {
+        /// SET VALUE IS IN VALID TOKEN TO FALSE AGAIN
+        _patient.isInvalidToken.value = false;
+        printDebug("REST INVALID TOKEN ${_patient.isInvalidToken.value}");
+
+        Get.offAll(const LoginScreen());
+        SnackBarMessage.showSnackBarMessage(
+            title: "Opps! ",
+            message: "Sesi anda telah habis, silahkan login kembali!",
+            typeMessage: SnackBarMessage.error);
+      });*/
+  }
+}
+
+void showFeedback() {
+  // Alerts.showAlertDialogFeedback(context);
+}
+// }
