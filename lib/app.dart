fae7a28d54072664efbd3868518e97cd1fbe3667
Muhammad Ziad Alfian <muhammadziadalfian07@gmail.com>
2023-02-07 12:53:46 +0800

    kanban-board#main Change Initial Page

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jd_mobile/common/routes/routes.dart';
 import 'package:jd_mobile/common/theme/theme.dart';
-import 'package:jd_mobile/persentation/pages/doctor/serach_doctor_page.dart';
 import 'package:jd_mobile/persentation/pages/global/splash_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.light,
       title: 'JumpaDokter',
       onGenerateRoute: AppRoutes.generateRoute,
-      home: const SearchDoctorPage(),
+      home: const SplashPage(),
     );
   }
 }
