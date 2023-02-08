a6d481fef1c751dd6d291305d19f6557ce430f37
Muhammad Ziad Alfian <muhammadziadalfian07@gmail.com>
2023-02-08 12:17:03 +0800

    Fix Issue Pipline

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ flutter pub get
 # flutter build appbundle --flavor develop -t lib/main_prod.dart -v
 
 # UNCOMENT WHEN BUILD PROD
-flutter build apk --release --flavor prod -t lib/main_prod.dart --split-per-abi
-flutter build appbundle --release --flavor prod -t lib/main_prod.dart
+# flutter build apk --release --flavor prod -t lib/main_prod.dart --split-per-abi
+# flutter build appbundle --release --flavor prod -t lib/main_prod.dart
+
+flutter build apk --release --flavor prod -t lib/main.dart --split-per-abi
+flutter build appbundle --release --flavor prod -t lib/main.dart
